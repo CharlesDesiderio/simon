94ae80d4d0ec89b6eee94dbf1427063baec38a2d
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-10-12 08:46:21 -0500

    Added user stories and psuedocode

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+
+// User can see a game display
+
+// User can see a 'start game' button
+
+// User can see an 'instructions' button.
+
+// Instructions button shows a modal with rules
+
+// Start game will initiate gameplay
+
+// Computer will play one move. Stores move in an array
+
+// Player repeats move.
+// On success, computer plays again.
+// On fail, game ends.
+
+// Computer picks new move, adds next move adds move to array. Replays sequence in array from start.
+
+// Player repeats sequence (game play loop ends here, go back to success/fail condition)
+
+
+
+
 $(() => {
   $('body').append('jquery loaded')
+
+
+
 })
