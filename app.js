0970aaf040d234bf25ceb71c06425aab355c372d
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-10-12 09:45:35 -0500

    Basic computer replay functionality

--- app.js
+++ app.js
@@ -20,11 +20,42 @@
 // Player repeats sequence (game play loop ends here, go back to success/fail condition)
 
 
+let computerMoves = [];
+let numberOfChoices = 4;
+
+// Base Game Logic
+const gameLoop = () => {
+
+  // Get computer move
+  getComputerMove();
+  console.log(computerMoves)
+
+  // Play computer moves
+  computerReplay(0)
+
+  // Wait for player input. Each button should have an event listener which passes target id to compare against computer move.
+
+
+}
+
+// Randomly select computer move
+const getComputerMove = () => {
+  computerMoves.push(Math.ceil(Math.random() * numberOfChoices))
+}
+
+// Replay computer pattern
+const computerReplay = (move) => {
+  if (move < computerMoves.length) {
+    $('#game').append(`<div>${computerMoves[move]}</div>`)
+    setTimeout(() => {
+      computerReplay(move + 1)
+    }, 500)
+  }
+}
 
 
 $(() => {
-  $('body').append('jquery loaded')
 
-
+  $('#startGame').on('click', gameLoop)
 
 })
