b597c23fa45c0544342cc71152a170db1d9774a4
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-10-15 22:47:01 -0500

    Added readme.md, cleaned up the code and remove console.logs

--- app.js
+++ app.js
@@ -17,14 +17,26 @@
 let computerMoves = [];
 let numberOfChoices = 4;
 let currentTurn = 0;
-let colorsArray = ['#003DB5', '#C30110', '#108047', '#E4B522']
-let congrats = ['Awesome!', 'Yeah!', 'You got it!', 'Pwned.', 'Killin\' it!', '#B4D455!'];
+let colorsArray = ["#003DB5", "#C30110", "#108047", "#E4B522"];
+let congrats = [
+  "Awesome!",
+  "Yeah!",
+  "You got it!",
+  "Pwned.",
+  "Killin' it!",
+  "#B4D455!",
+];
 // Used grepper's site to understand how to easily play audio: https://www.codegrepper.com/code-examples/fortran/jquery+play+audio
-let audioArr = ['smw_swimming.wav', 'smw_fireball.wav', 'smw_kick.wav', 'smw_coin.wav']
+let audioArr = [
+  "smw_swimming.wav",
+  "smw_fireball.wav",
+  "smw_kick.wav",
+  "smw_coin.wav",
+];
 // Initial set of colors from flatuicolors.com
 // let colorsArray = ['#e74c3c', '#f1c40f', '#2980b9', '#27ae60', '#d35400', '#34495e', '#8e44ad']
 let isComputerTurn = false;
-let scoresArr = localStorage.getItem('localHighScores') || [];
+let scoresArr = localStorage.getItem("localHighScores") || [];
 let score = 0;
 let awaitPlayer = false;
 
@@ -33,7 +45,7 @@ const gameLoop = () => {
   if (awaitPlayer === true) {
     return;
   }
-  $('#message').empty() // Clear any previous 'Game Over' message
+  $("#message").empty(); // Clear any previous 'Game Over' message
 
   // Get computer move
   getComputerMove();
@@ -42,59 +54,64 @@ const gameLoop = () => {
   generateBoard();
 
   // Replay computer moves for player to copy.
-  computerReplay(0)
+  computerReplay(0);
 
   // Event handlers on squares will handle the logic for tracking the player move and will restart the game loop when the player matches all the correct moves.
-}
+};
 
 // Create the game board.
 const generateBoard = () => {
-  $('#game').empty();
+  $("#game").empty();
   for (let i = 0; i < numberOfChoices; i++) {
-    let $bgDiv = $('<div>')
-    $bgDiv.addClass('bg-div')
-    let $colorButton = $('<button>')
-    $colorButton.addClass('colorButton')
-    
+    let $bgDiv = $("<div>");
+    $bgDiv.addClass("bg-div");
+    let $colorButton = $("<button>");
+    $colorButton.addClass("colorButton");
+
     // Chose a color from array of colors, If there are more buttons than colors, pick a random color
     // Currently, this feature isn't used as the game only generates four buttons.
     if (i < colorsArray.length) {
-      $colorButton.css('background-color', colorsArray[i])
+      $colorButton.css("background-color", colorsArray[i]);
     } else {
-      $colorButton.css('background-color', `rgba(${randomColor()}, ${randomColor()}, ${randomColor()})`)
+      $colorButton.css(
+        "background-color",
+        `rgba(${randomColor()}, ${randomColor()}, ${randomColor()})`
+      );
     }
-    $colorButton.attr('id', i) // Identify which button is which. The internal logic doesn't care about the colors, that will all be done with CSS
-    $colorButton.on('click', choice)
-    $($bgDiv).append($colorButton)
-    $('#game').append($bgDiv)
+    $colorButton.attr("id", i); // Identify which button is which. The internal logic doesn't care about the colors, that will all be done with CSS
+    $colorButton.on("click", choice);
+    $($bgDiv).append($colorButton);
+    $("#game").append($bgDiv);
   }
-  
+
   // Build the gray backgrounds that connect the 'rows' of buttons
-  $('#game').children().eq(0).css('border-radius', '50% 50% 0 0')
-  $('#game').children().eq(1).css('border-radius', '50% 50% 0 0')
-  $('#game').children().eq(2).css('border-radius', '0 0 50% 50%')
-  $('#game').children().eq(3).css('border-radius', '0 0 50% 50%')
-}
+  $("#game").children().eq(0).css("border-radius", "50% 50% 0 0");
+  $("#game").children().eq(1).css("border-radius", "50% 50% 0 0");
+  $("#game").children().eq(2).css("border-radius", "0 0 50% 50%");
+  $("#game").children().eq(3).css("border-radius", "0 0 50% 50%");
+};
 
 // Map the high scores to the correct element
 const buildScores = () => {
-  $('#score').empty()
-  $('#score').html(`<h3>High Scores:</h3>`)
+  $("#score").empty();
+  $("#score").html(`<h3>High Scores:</h3>`);
   // Check if the array is a string. If it is, make it an array. This is important because localStorage only uses strings
-  if (typeof scoresArr === "string") scoresArr = scoresArr.split(",")
+  if (typeof scoresArr === "string") scoresArr = scoresArr.split(",");
   // Just need the top ten scores, not all of them. Snip snip.
   if (scoresArr.length > 10) {
-    scoresArr = scoresArr.sort((a, b) => b - a).slice(0, 10)
+    scoresArr = scoresArr.sort((a, b) => b - a).slice(0, 10);
   }
-  scoresArr.sort((a, b) => b - a).map((currentScore) => {
-    $('#score').append(`${currentScore} <br />`)
-  })
-}
+  scoresArr
+    .sort((a, b) => b - a)
+    .map((currentScore) => {
+      $("#score").append(`${currentScore} <br />`);
+    });
+};
 
 // Randomly select computer move
 const getComputerMove = () => {
-  computerMoves.push(Math.floor(Math.random() * numberOfChoices))
-}
+  computerMoves.push(Math.floor(Math.random() * numberOfChoices));
+};
 
 // Replay computer pattern
 const computerReplay = (move) => {
@@ -104,35 +121,39 @@ const computerReplay = (move) => {
 
     // Found information about the CSS brightness filter (as well as opacity) and its use without preprocessors here: https://stackoverflow.com/questions/1625681/dynamically-change-color-to-lighter-or-darker-by-percentage-css-javascript
 
-    $('#computerMoveDisplay').empty()
-    const $displayIcon = $('<div>')
-    $displayIcon.addClass('displayIcon')
-    $displayIcon.attr('id', computerMoves[move])
-    $displayIcon.css('background-color', colorsArray[computerMoves[move]])
-    $('#computerMoveDisplay').append($displayIcon)
+    $("#computerMoveDisplay").empty();
+    const $displayIcon = $("<div>");
+    $displayIcon.addClass("displayIcon");
+    $displayIcon.attr("id", computerMoves[move]);
+    $displayIcon.css("background-color", colorsArray[computerMoves[move]]);
+    $("#computerMoveDisplay").append($displayIcon);
 
     // Show the move to the player for 350ms, then blank the display for 150ms before the next item is shown
     setTimeout(() => {
-      $('#computerMoveDisplay').empty()
-    }, 350)
+      $("#computerMoveDisplay").empty();
+    }, 350);
     setTimeout(() => {
-      computerReplay(move + 1)
-    }, 600)
+      computerReplay(move + 1);
+    }, 600);
   } else {
-    isComputerTurn = false
+    isComputerTurn = false;
     awaitPlayer = true;
   }
-}
+};
 
 // Manage input from player on color square click
 const choice = (event) => {
   if (isComputerTurn === true) {
     return;
   }
-  let buttonSound = new Audio(audioArr[parseInt($(event.target).attr('id'))])
-  buttonSound.play()
-  $('#message').html(`<h1>${congrats[Math.floor(Math.random() * congrats.length)]}!</h1>`)
-  if (parseInt($(event.currentTarget).attr('id')) === computerMoves[currentTurn]) {
+  let buttonSound = new Audio(audioArr[parseInt($(event.target).attr("id"))]);
+  buttonSound.play();
+  $("#message").html(
+    `<h1>${congrats[Math.floor(Math.random() * congrats.length)]}!</h1>`
+  );
+  if (
+    parseInt($(event.currentTarget).attr("id")) === computerMoves[currentTurn]
+  ) {
     currentTurn++;
     if (currentTurn === computerMoves.length) {
       score++;
@@ -142,57 +163,56 @@ const choice = (event) => {
     }
   } else {
     // Reset game here!
-    $('#message').html(`<h1>Game Over. Score: ${score}</h1>`)
-    let gameOver = new Audio('smw_game_over.wav')
-    gameOver.play()
-    $('#computerMoveDisplay').empty().append(`❌`)
-    $('#game').empty()
+    $("#message").html(`<h1>Game Over. Score: ${score}</h1>`);
+    let gameOver = new Audio("smw_game_over.wav");
+    gameOver.play();
+    $("#computerMoveDisplay").empty().append(`❌`);
+    $("#game").empty();
     // Add score to array.
-    if (score > 0) scoresArr.push(score)
-    localStorage.setItem('localHighScores', scoresArr)
+    if (score > 0) scoresArr.push(score);
+    localStorage.setItem("localHighScores", scoresArr);
     // Sort scores descending and display them
 
-    buildScores()
+    buildScores();
 
-    resetGame()
-    $('#start').css('display', 'block')
+    resetGame();
+    $("#start").css("display", "block");
   }
-}
+};
 
 const randomColor = () => {
-  return Math.floor(Math.random() * 256)
-}
+  return Math.floor(Math.random() * 256);
+};
 
 const resetGame = () => {
   computerMoves = [];
   currentTurn = 0;
   score = 0;
   awaitPlayer = false;
-}
+};
 
-buildScores()
+buildScores();
 
 $(() => {
-
   // Modal display control.
-  $('#showRules').on('click', () => {
-    $('#rules').css('display', 'block')
-  })
+  $("#showRules").on("click", () => {
+    $("#rules").css("display", "block");
+  });
 
   // Modal close button control.
-  $('#byeModal').on('click', () => {
-    $('#rules').css('display', 'none')
-  })
+  $("#byeModal").on("click", () => {
+    $("#rules").css("display", "none");
+  });
 
-  $('#startGame').on('click', () => {
+  $("#startGame").on("click", () => {
     isComputerTurn = true;
-    let newGameSound = new Audio('smw_1-up.wav')
+    let newGameSound = new Audio("smw_1-up.wav");
     newGameSound.play();
     setTimeout(() => {
-      gameLoop()
-    }, 500)
-  }) 
-})
+      gameLoop();
+    }, 500);
+  });
+});
 
 // Mainly for future reference: https://stackoverflow.com/questions/12813573/position-icons-into-circle and http://jsfiddle.net/ThiefMaster/LPh33/4/
 // Dynamically generate a ring of elements. This is something I may come back to for my stretch goal.
